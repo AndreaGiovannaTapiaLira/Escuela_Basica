b1d93f0a007280961dd2daa964f195eadb03d512
StefanyBG08 <20253RD024@uetz.edu.mx>
2026-03-04 14:03:26 -0600

    Programa corregido

--- Escuela.java
+++ Escuela.java
@@ -1,32 +1,37 @@
-public class Escuela extends Alumno{
+public class Escuela {
 
     private Alumno alumno1;
     private Alumno alumno2;
     private Alumno alumno3;
     private Alumno alumno4;
     private Alumno alumno5;
-    private int contador;
 
-    public Escuela() {
-        contador = 0;
-    }
+    private int contador = 0;  // ← Inicializado aquí directamente
 
     public void agregarAlumno(Alumno alumno) {
-        if (contador == 0) {
-            alumno1 = alumno;
-        } else if (contador == 1) {
-            alumno2 = alumno;
-        } else if (contador == 2) {
-            alumno3 = alumno;
-        } else if (contador == 3) {
-            alumno4 = alumno;
-        } else if (contador == 4) {
-            alumno5 = alumno;
-        } else {
+
+        if (contador >= 5) {
             System.out.println("No hay espacio para más alumnos.");
             return;
         }
-        contador++;
+
+        if (contador == 0) {
+            alumno1 = alumno;
+        } 
+        else if (contador == 1) {
+            alumno2 = alumno;
+        } 
+        else if (contador == 2) {
+            alumno3 = alumno;
+        } 
+        else if (contador == 3) {
+            alumno4 = alumno;
+        } 
+        else if (contador == 4) {
+            alumno5 = alumno;
+        }
+
+        contador++; // ← Se incrementa al final
         System.out.println("Alumno agregado correctamente.");
     }
 
