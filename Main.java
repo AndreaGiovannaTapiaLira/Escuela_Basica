9064f009c7a209d76e7aaa3b35e86d559d95bfc3
StefanyBG08 <20253RD024@uetz.edu.mx>
2026-03-04 13:47:15 -0600

    Programa del main

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Escuela escuela = new Escuela();
+
+        Alumno a1 = new Alumno("Ana", "A001", 9, 8, 10);
+        Alumno a2 = new Alumno("Luis", "A002", 7, 9, 8);
+
+        escuela.agregarAlumno(a1);
+        escuela.agregarAlumno(a2);
+
+        System.out.println("\nLista de alumnos:");
+        escuela.mostrarAlumnos();
+
+        System.out.println("\nBuscar alumno A001:");
+        Alumno encontrado = escuela.buscarAlumno("A001");
+
+        if (encontrado != null) {
+            encontrado.mostrarAlumno();
+        } else {
+            System.out.println("Alumno no encontrado.");
+        }
+    }
+}
